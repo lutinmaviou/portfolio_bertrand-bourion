56ce32aa116ecb440454cf8f4664e80afd3e1b5d
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-07-22 13:39:48 +0200

    setup application

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Bertrand Bourion | Portfolio</title>
         <meta
           name="Portfolio Bertrand Bourion"
           content="Mon portfolio créé avec Next.js"
