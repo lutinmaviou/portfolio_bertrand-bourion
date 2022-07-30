ad00c0b5c0f5d9c06e1867eebb21264abac10b01
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-07-30 16:18:30 +0200

    style: fix final animations on home page

--- src/gsap/comingMoon.js
+++ src/gsap/comingMoon.js
@@ -2,7 +2,11 @@ import { gsap } from 'gsap';
 
 const comingMoon = () => {
   const moon = document.querySelector('.moon');
-  gsap.fromTo(moon, { scale: 0.4 }, { scale: 1, duration: 20, ease: 'power1' });
+  gsap.fromTo(
+    moon,
+    { scale: 0.4, left: '65%' },
+    { scale: 1, left: '60%', duration: 30, ease: 'power1' }
+  );
 };
 
 export default comingMoon;
