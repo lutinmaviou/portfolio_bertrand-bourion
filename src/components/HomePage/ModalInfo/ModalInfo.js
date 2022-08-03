959fc8d7b3b9bac9c80fffa48e6d2db048a3fcc8
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-08-03 17:13:07 +0200

    feat(Modals): display text on Question marks hover

--- src/components/HomePage/ModalInfo/ModalInfo.js
+++ src/components/HomePage/ModalInfo/ModalInfo.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ModalBox from './ModalBox';
 import ModalQuestionMark from './ModalQuestionMark';
 
 const ModalInfo = () => {
+  useEffect(() => {
+    const boatModal = document.querySelector('.boat-modal');
+    const seaModal = document.querySelector('.sea-modal');
+    const moonModal = document.querySelector('.moon-modal');
+    const boatModalBox = document.querySelector('.boat-modalBox');
+    const seaModalBox = document.querySelector('.sea-modalBox');
+    const moonModalBox = document.querySelector('.moon-modalBox');
+    boatModal.addEventListener('mouseenter', () => (boatModalBox.style.opacity = 1));
+    boatModal.addEventListener('mouseleave', () => (boatModalBox.style.opacity = 0));
+    seaModal.addEventListener('mouseenter', () => (seaModalBox.style.opacity = 1));
+    seaModal.addEventListener('mouseleave', () => (seaModalBox.style.opacity = 0));
+    moonModal.addEventListener('mouseenter', () => (moonModalBox.style.opacity = 1));
+    moonModal.addEventListener('mouseleave', () => (moonModalBox.style.opacity = 0));
+  });
   return (
     <div className="modalInfo-container">
       <ModalQuestionMark className="boat-modal" />
