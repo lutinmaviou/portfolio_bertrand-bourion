d809e9cfdce17fde19cadca8601eb9036112d649
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-08-08 17:57:19 +0200

    feat(navbar): add external links to GitHub and LinkedIn

--- src/components/Navbars/Navbar.js
+++ src/components/Navbars/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { buildUrl } from 'cloudinary-build-url';
+import cloudName from '../../utils/cloudinary/cloudName';
 
 const Navbar = () => {
   const router = useRouter();
+  const repo = 'portfolio-next/';
+  const GitHub = buildUrl(`${repo}github-logo_djzd6z`, cloudName);
+  const LinkedIn = buildUrl(`${repo}linkedin-logo_rvtgg4`, cloudName);
   return (
     <header className="navbar-container flex-between">
       <div className="flex-col items-center justify-center">
@@ -22,6 +28,30 @@ const Navbar = () => {
             </li>
             <li className={router.pathname === '/projects' ? 'active' : ''}>
               <Link href="/projects">Projects</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <nav className="external-links flex-col justify-center surfer">
+        <div className="nav-items">
+          <ul className="flex-around">
+            <li>
+              <Image
+                src={GitHub}
+                alt="Logo GitHub"
+                layout="intrinsic"
+                width="60"
+                height="50"
+              />
+            </li>
+            <li>
+              <Image
+                src={LinkedIn}
+                alt="Logo GitHub"
+                layout="intrinsic"
+                width="60"
+                height="60"
+              />
             </li>
           </ul>
         </div>
