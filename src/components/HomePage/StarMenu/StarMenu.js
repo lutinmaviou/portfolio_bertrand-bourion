da285c387d10416b08481316a5e4694e3144c09a
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-10-04 16:41:22 +0200

    style(home page): change font family for nav

--- src/components/HomePage/StarMenu/StarMenu.js
+++ src/components/HomePage/StarMenu/StarMenu.js
@@ -2,8 +2,7 @@ import Link from 'next/link';
 
 const StarMenu = () => {
   return (
-    <nav className="starNav-container">
-      {/* <nav className="star-nav surfer"> */}
+    <nav className="starNav-container surfer">
         <ul>
           <li>
             <Link href="/about">about me</Link>
@@ -13,7 +12,6 @@ const StarMenu = () => {
           </li>
           <li>contact me</li>
         </ul>
-      {/* </nav> */}
     </nav>
   );
 };
